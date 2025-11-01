863cb86b58d9fb4e417d0a4f887696c297a8f789
Moritz Wesemann <mweseman@usc.edu>
2025-11-01 08:39:11 -0700

    added a simple card in the middle of the screen to show the question

---
+++ app/components/QuestionCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function QuestionCard() {
+  return (
+    <div className="min-h-dvh flex items-center justify-center px-4 ">
+      <div className="w-full max-w-md border rounded-xl p-4 bg-white">
+        <h1>How was your day!</h1>
+        <p>Year: 2020</p>
+        <p>Episode Number: 123</p>
+        <p>Question By: Felix</p>
+      </div>
+    </div>
+  );
+}
